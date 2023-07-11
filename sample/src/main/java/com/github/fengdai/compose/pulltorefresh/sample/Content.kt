d9e25b5da8d3ce45f905eaf3b4355d327aae8aa8
Morgan Rewis <rewismorgan@gmail.com>
2023-07-11 18:04:06 -0400

    Back Button Works

--- sample/src/main/java/com/github/fengdai/compose/pulltorefresh/sample/Content.kt
+++ sample/src/main/java/com/github/fengdai/compose/pulltorefresh/sample/Content.kt
@@ -1,8 +1,10 @@
 package com.github.fengdai.compose.pulltorefresh.sample
 
+import android.graphics.Bitmap
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +15,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -27,8 +33,12 @@ fun Content(contentPadding: PaddingValues, refreshing: Boolean) {
         verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
         items(1) { index ->
+            var backEnabled by remember { mutableStateOf(false) }
+            var webView: WebView? = null
+
             // Declare a string that contains a url
-            val mUrl = "https://www.youtube.com"
+            val mUrl = "https://www.raddle.me"
+            //val mUrl = "https://www.youtube.com"
 
             // Adding a WebView inside AndroidView
             // with layout as full screen
@@ -39,15 +49,26 @@ fun Content(contentPadding: PaddingValues, refreshing: Boolean) {
                         ViewGroup.LayoutParams.MATCH_PARENT
                     )
                     webViewClient = WebViewClient()
-                    settings.javaScriptEnabled = true
+
+                    webViewClient = object : WebViewClient() {
+                        override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                            backEnabled = view.canGoBack()
+                        }
+                    }
+
+                    //settings.javaScriptEnabled = true
                     loadUrl(mUrl)
-                    reload()
+                    webView = this
                 }
             }, update = {
+                webView = it
                 if (refreshing) {
                     it.reload()
                 }
             })
+            BackHandler(enabled = backEnabled) {
+                webView?.goBack()
+            }
         }
     }
 }
