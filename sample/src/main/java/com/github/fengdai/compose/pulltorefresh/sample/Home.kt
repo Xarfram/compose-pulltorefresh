8d19dadcf85313cac95f188cf98c9cae57dcbdeb
Morgan Rewis <rewismorgan@gmail.com>
2023-07-11 23:18:30 -0400

    Transparent top bar

--- sample/src/main/java/com/github/fengdai/compose/pulltorefresh/sample/Home.kt
+++ sample/src/main/java/com/github/fengdai/compose/pulltorefresh/sample/Home.kt
@@ -1,7 +1,9 @@
 package com.github.fengdai.compose.pulltorefresh.sample
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -24,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,7 +45,9 @@ fun Home(navController: NavController) {
     Scaffold(
         topBar = {
             SmallTopAppBar(
-                title = { Text("Raddle") },
+                title = { Text("") },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Transparent )
             )
         }
     ) { padding ->
